4fa046caa790b8a357142f68b8fe141d48f11b3b
bdwalker93 <bdwalker93@yahoo.com>
2016-04-28 22:21:52 -0700

    updated the SQL create string. Added the order_table information

--- inf124grp17.sql
+++ inf124grp17.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 28, 2016 at 10:15 AM
+-- Generation Time: Apr 29, 2016 at 07:20 AM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 7.0.5
 
@@ -19,6 +19,30 @@ SET time_zone = "+00:00";
 --
 -- Database: `inf124grp17`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_information`
+--
+
+CREATE TABLE `order_information` (
+  `order_id` int(11) NOT NULL,
+  `product_id` int(50) NOT NULL,
+  `size` varchar(50) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `phone_number` varchar(50) NOT NULL,
+  `street` varchar(50) NOT NULL,
+  `city` varchar(16) NOT NULL,
+  `state` varchar(5) NOT NULL,
+  `zip_code` varchar(2) NOT NULL,
+  `shipping_type` varchar(50) NOT NULL,
+  `credit_card_number` varchar(25) NOT NULL,
+  `credit_card_expiration` varchar(20) NOT NULL,
+  `notes` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -42,19 +66,6 @@ CREATE TABLE `product_descriptions` (
 
 INSERT INTO `product_descriptions` (`id`, `title`, `brand`, `name`, `description`, `price`, `image_path`) VALUES
 (1234, 'Samsung - S2 Smart Watch', 'Samsung', 'S2 Smart Watch', '        With elegant curves and premium finishes, the Samsung Gear S2 will turn heads. The intuitive circular face and bezel let you navigate effortlessly to get to what you need. And since it pairs with your phone, it''s easy to gain access to important notifications, texts and updates with just a glance at your wrist. With built-in wireless charging, it''s easy to keep the Gear S2 powered up anywhere by simply setting your Gear S2 on the included wireless charging dock. What''s even better, is that the The Gear S2 is compatible with most Android devices so you can get in on the experience.', 900, 'images/product_images/samsung.jpg');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `table 3`
---
-
-CREATE TABLE `table 3` (
-  `COL 1` varchar(5) DEFAULT NULL,
-  `COL 2` varchar(7) DEFAULT NULL,
-  `COL 3` varchar(89) DEFAULT NULL,
-  `COL 4` varchar(12) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -69218,11 +69229,26 @@ INSERT INTO `zip_codes` (`zip`, `state`, `city`) VALUES
 --
 
 --
+-- Indexes for table `order_information`
+--
+ALTER TABLE `order_information`
+  ADD PRIMARY KEY (`order_id`);
+
+--
 -- Indexes for table `product_descriptions`
 --
 ALTER TABLE `product_descriptions`
   ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `order_information`
+--
+ALTER TABLE `order_information`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
