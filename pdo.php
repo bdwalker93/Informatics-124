1bef0f3c1c4396a088d866e6a8993ca32cb960ba
bdwalker93 <bdwalker93@yahoo.com>
2016-05-01 21:55:08 -0700

    added try catch to server connecton and made it production ready

--- pdo.php
+++ pdo.php
@@ -4,20 +4,25 @@
  *  The purpose of this file is to connect to a MYSQL databse with the variable
  *      "pdo". Requiring this file allows other php files to use this pdo variable
 */
+
     //for production
-//    $db_server_name = "instdav.ics.uci.edu";
-//    $db_name = "inf124grp17";
-//    $db_user_name = "inf124grp17";
-//    $db_password = "4ru&RuHU";
-    
-    //for testing
-    $db_server_name = "localhost";
+    $db_server_name = "sylvester-mccoy-v3.ics.uci.edu";
     $db_name = "inf124grp17";
-    $db_user_name = "root";
-    $db_password = "";
-    //connects to the db
-    $conn = new PDO("mysql:host=$db_server_name;dbname=$db_name", $db_user_name, $db_password);
-        
-    //sets the error mode attribute to throw exceptions
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db_user_name = "inf124grp17";
+    $db_password = "4ru&RuHU";
+
+    try 
+    {
+
+        //connects to the db
+        $conn = new PDO("mysql:host=$db_server_name;dbname=$db_name", $db_user_name, $db_password);
+
+        //sets the error mode attribute to throw exceptions
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    catch (PDOException $e) {
+       exit("DB ERROR!");
+    }
+
 ?>
